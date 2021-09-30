74b3480478b7454547b429f2238d2a11a93b6bd4
yisas2112 <rodriguezjesu1992@gmail.com>
2021-09-30 13:10:04 -0300

    Creación de funcion de texto que dice que el carrito está vacío

--- js/carrito.js
+++ js/carrito.js
@@ -2,43 +2,48 @@ let contenedor = document.getElementById('main__carrito');
 let produtosElegidos =  JSON.parse(localStorage.getItem('carrito'))
 console.log(produtosElegidos)
 
+produtosElegidos.length > 0 ? mostrarProductos() : CarritoVacio()
 
+function mostrarProductos(){
+        for(const produ of produtosElegidos){        
+            console.log(produ.length)
+            
+            let principal = document.createElement('div');
+            $(principal).addClass( "card__products" );
+            $(principal).addClass("col-3");
+            $(principal).addClass( "text-center");
+            
+            
+                principal.innerHTML = `
+                <div id=${produ.id} class="card" style="width: 18rem;">
+                        <img src='${produ.img}' class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${produ.nombre}</h5>
+                            <p class="card-text">Marca: ${produ.marca}</p>
+                            <p class="card-text"> $${produ.precio}</p>
+                            <p class="card-text"> Cantidad: ${produ.cantidad}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">                            
+                            <li class="list-group-item">Total: $${produ.total}</li>
+                        </ul>
+                        <div class="card-body">                
+                                <button href="#" class="card-link" onclick={EliminarProducto(${produ.id})}>Eliminar Producto</button>
+                        </div>            
+                </div>    
+                `;
+            contenedor.appendChild(principal)   
+        }
+}
 
-
-
-for(const produ of produtosElegidos){        
-    
+function CarritoVacio (){
     let principal = document.createElement('div');
-    $(principal).addClass( "card__products" );
-    $(principal).addClass("col-3");
-    $(principal).addClass( "text-center");
-    
-    
-    
-    principal.innerHTML = `
-    <div id=${produ.id} class="card" style="width: 18rem;">
-            <img src='${produ.img}' class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${produ.nombre}</h5>
-                <p class="card-text">Marca: ${produ.marca}</p>
-                <p class="card-text"> $${produ.precio}</p>
-                <p class="card-text"> Cantidad: ${produ.cantidad}</p>
-            </div>
-            <ul class="list-group list-group-flush">                            
-                <li class="list-group-item">Total: $${produ.total}</li>
-            </ul>
-            <div class="card-body">                
-                    <button href="#" class="card-link" onclick={EliminarProducto(${produ.id})}>Eliminar Producto</button>
-                    
-                    
-                    
-                </div>            
-    </div>    
-    `;
+            $(principal).addClass( "card__products" );
+            $(principal).addClass("col-3");
+            $(principal).addClass( "text-center");
 
-    contenedor.appendChild(principal)   
+    principal.innerHTML = `<h1>Para poder ingresar a la página debe ser mayor de Edad</h1>`;
 
-    
+    contenedor.appendChild(principal)  
 }
 
 
@@ -50,16 +55,9 @@ const EliminarProducto = (id)=>{
         if(produtosElegidos[i].id == id ){
             produtosElegidos.splice(i, 1);
         }
-    }
-    
+    }    
     document.getElementById(id).remove()
     localStorage.setItem('carrito',JSON.stringify(produtosElegidos))
     
 }
 
-
-const BorrarProductoDom = ()=>{
-    for(const produ of produtosElegidos){
-        
-    }
-}
