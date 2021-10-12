86317c57706a6c0968e4f1687e4bab4f6f4646fb
yisas2112 <rodriguezjesu1992@gmail.com>
2021-10-12 11:17:19 -0300

    se terminó de crear el array de compra

--- js/formulario.js
+++ js/formulario.js
@@ -1,19 +1,39 @@
 let produtosElegidos =  JSON.parse(localStorage.getItem('carrito'))
-
+console.log(produtosElegidos)
 
 
 class Persona{
-    constructor(nombreCompleto, email, domicilio, numDomi, localidad, partido, productos){
+    constructor(nombreCompleto, email, domicilio, numDomi, localidad, codigoPostal, productos){
         this.nombreCompleto = nombreCompleto;
         this.email = email;
         this.domicilio = domicilio;
         this.numDomi = numDomi;
         this.Localidad = localidad;
-        this.partido = partido;
+        this.codigoPostal = codigoPostal
         this.productos = [productos]
     }
 
-    OrdenDeCompra(nombreCompleto, email,domicilio,numDomi,localidad,partido, productos){
+    OrdenDeCompra(nombreCompleto, email,domicilio,numDomi,localidad, productos){
         
     }
 }
+
+
+
+
+let form = document.getElementById('formulario')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    let formData = new FormData(form)    
+    
+    let prueba = new Persona(formData.get('nombre_apellido'),formData.get('mail'),formData.get('domicilio'),formData.get('numero'),formData.get('localidad'), formData.get('codigoPostal'), JSON.parse(localStorage.getItem('carrito')) )
+
+    console.log(prueba)
+
+     
+})
+
+
+
+
